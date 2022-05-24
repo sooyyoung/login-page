432e37177b3a317a90ffe39bd655a1d7aedfae3c
sooyyoung <sooyyyoung@gmail.com>
2022-05-24 10:47:26 +0900

    modal창 open, close 코드 변경

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 // 로그인 modal창 열기, 닫기
-function modalOpen() {
-    document.querySelector('.modal-box').style.display = 'block';
-}
-function modalClose() {
-    document.querySelector('.modal-box').style.display = 'none';
-}
+
+const modalOpen = document.querySelector('.login-link');
+const modalClose = document.querySelector('.close');
+const modalBox = document.querySelector('.modal-box');
+
+modalOpen.addEventListener('click', function(){
+    modalBox.style.display = 'block';
+})
+
+modalClose.addEventListener('click', function(){
+    modalBox.style.display = 'none';
+})
