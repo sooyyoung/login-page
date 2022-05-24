2139291a4f357a124cbc76f0aaba0b4bc1b9599e
sooyyoung <sooyyyoung@gmail.com>
2022-05-24 10:03:49 +0900

    js 파일 분리

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// 로그인 modal창 열기, 닫기
+function modalOpen() {
+    document.querySelector('.modal-box').style.display = 'block';
+}
+function modalClose() {
+    document.querySelector('.modal-box').style.display = 'none';
+}
