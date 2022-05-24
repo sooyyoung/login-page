ec0ad34d922663d69df3d38fdb53450ca6d020e8
sooyyoung <sooyyyoung@gmail.com>
2022-05-24 18:01:53 +0900

    로그인 기능 추가

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
-// 로그인 modal창 열기, 닫기
-
 const modalOpen = document.querySelector('.login-link');
 const modalClose = document.querySelector('.close');
 const modalBox = document.querySelector('.modal-box');
 
+const loginId = document.querySelector('.userID');
+const loginPw = document.querySelector('.userPW');
+const loginBtn = document.querySelector('.login-btn');
+
+// 로그인 modal창 열기, 닫기
 modalOpen.addEventListener('click', function(){
     modalBox.style.display = 'block';
 })
@@ -11,3 +14,17 @@ modalOpen.addEventListener('click', function(){
 modalClose.addEventListener('click', function(){
     modalBox.style.display = 'none';
 })
+
+// 로그인 기능 
+loginBtn.addEventListener('click', signIn);
+
+function signIn(){
+    if(loginId.value == ""){
+        return alert('아이디를 입력하세요');
+    } else if(loginPw.value == ""){
+        return alert('비밀번호를 입력하세요');
+    } else {
+        return alert('로그인 성공!');
+    }
+}
+
